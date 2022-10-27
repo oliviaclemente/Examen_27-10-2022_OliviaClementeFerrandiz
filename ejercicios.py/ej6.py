1210b2cb5ae50f2a3f28586321d29db7023cf4a4
oliviaclemente <113981873+oliviaclemente@users.noreply.github.com>
2022-10-27 08:58:30 +0000

    ccc

--- ejercicios.py/ej6.py
+++ ejercicios.py/ej6.py
@@ -6,4 +6,34 @@ funcion_modificar.remove(0)
 funcion_modificar.remove(3)
 print("funcion_modificar=", funcion_modificar)
 
-def separar 
+def separar(funcion_modificar1):
+  pares= []
+  impares= []
+  for i in funcion_modificar1:
+    if i%2 == 0:
+      pares.append(i)
+    else: 
+      impares.append(i)
+    pares.sort()
+    impares.sort()
+    return pares, impares
+  funcion_modificar1 = [funcion_modificar]
+  pares,impares= separar(funcion_modificar)
+  print("Pares:", pares)
+
+suma_funcion=0
+for funcion_modificar1 in funcion_modificar:
+  suma_funcion += funcion_modificar1
+print(suma_funcion)
+
+pares.insert(0,suma_funcion)
+print(pares)
+
+suma_funcion = 0
+for funcion in pares[2::]:
+  suma_funcion += funcion_modificar
+print(funcion_modificar)
+print(pares[0] == pares[-1::])
+
+
+
